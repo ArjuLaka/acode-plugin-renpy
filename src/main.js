4694456ebdabdf664e189ef69a5afe36d5367472
ArjuLaka <arjulaka14@gmail.com>
2023-09-28 11:15:20 +0700

    upload

--- src/main.js
+++ src/main.js
@@ -1,24 +1,61 @@
+import './renpy_highlight_rules';
+
+import renpyIcon from '../icons.png';
 import plugin from '../plugin.json';
 
+const Url = acode.require('url');
+const { addMode, removeMode } = acode.require('aceModes');
+
 class AcodePlugin {
+  #style;
 
-  async init() {
+  async init(firstTime) {
+    this.#style = <style
+      textContent={
+        `.file_type_renpy::before{
+          display: inline-block;
+          content: '';
+          background-image: url(${renpyIcon});
+          background-size: contain;
+          background-repeat: no-repeat;
+          height: 1em;
+          width: 1em;
+        }`
+      }
+    ></style>
 
+
+    addMode('renpy', ['rpy', 'rpym'], "Ren'Py");
+    document.head.append(this.#style);
+
+    if (!firstTime) return;
+
+    editorManager.files.forEach(file => {
+      // update session mode
+      if (Url.extname(file.name) === '.rpy' || '.rpym') {
+        file.session.setMode('ace/mode/renpy');
+      }
+    });
   }
 
   async destroy() {
+    this.#style.remove();
+    removeMode('renpy');
 
+    editorManager.files.forEach(file => {
+      // update session mode
+      if (Url.extname(file.name) === '.rpy' || '.rpym') {
+        file.session.setMode('ace/mode/text');
+      }
+    });
   }
+
 }
 
 if (window.acode) {
   const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-    if (!baseUrl.endsWith('/')) {
-      baseUrl += '/';
-    }
-    acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init($page, cacheFile, cacheFileUrl);
+  acode.setPluginInit(plugin.id, (baseUrl, $page, options) => {
+    acodePlugin.init(options.firstInit);
   });
   acode.setPluginUnmount(plugin.id, () => {
     acodePlugin.destroy();
